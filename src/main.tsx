51dba13eed212a00e138065e4cfb818fe288b10c
Nikolai Maslak <nikomaslak@gmail.com>
2026-08-16 02:26:47 -0700

    add invariant function to validate root element existence

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
+import { invariant } from './invariant'
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')
+invariant(root, 'Root element not found')
+
+createRoot(root).render(
   <StrictMode>
     <App />
   </StrictMode>,
