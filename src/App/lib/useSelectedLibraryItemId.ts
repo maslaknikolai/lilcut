b9ca4eb2f000e94a9d82f407cb5acfd884b6a053
Nikolai Maslak <nikomaslak@gmail.com>
2026-08-26 05:57:28 -0700

    Open sidebar when selecting a library item

--- src/App/lib/useSelectedLibraryItemId.ts
+++ src/App/lib/useSelectedLibraryItemId.ts
@@ -1,13 +1,17 @@
+import { useSetAtom } from 'jotai'
 import { useNavigate, useParams } from 'react-router-dom'
+import { isSidebarOpenAtom } from '@/App/atoms'
 
 export function useSelectedLibraryItemId() {
   const { selectedId } = useParams<{ selectedId?: string }>()
   const navigate = useNavigate()
+  const setIsSidebarOpen = useSetAtom(isSidebarOpenAtom)
 
   const selectedLibraryItemId = selectedId ? decodeURIComponent(selectedId) : null
 
   function setSelectedLibraryItemId(id: string | null) {
     navigate(id ? `/${encodeURIComponent(id)}` : '/')
+    setIsSidebarOpen(true)
   }
 
   return [selectedLibraryItemId, setSelectedLibraryItemId] as const
